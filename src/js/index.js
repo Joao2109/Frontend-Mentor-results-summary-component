cc953782c6af7169fed58b5fb24b4998e85325b8
João Felipe <116182164+Joao2109@users.noreply.github.com>
2024-01-25 10:59:02 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+anime ({
+    targets: '#result-context',
+    width: '100px',
+    height: '100px'
+});
+anime ({
+    targets: '#result-context h1',
+    fontSize: '50px',
+    lineHeight: '50px'
+});
+anime ({
+    targets: '#result-context h6',
+    fontSize: '15px',
+    lineHeight: '30px'
+});
+anime ({
+    targets: '#result h2',
+    fontSize: '1.5em',
+    lineHeight: '1.5em'
+});
+anime ({
+    targets: '#result-context',
+    width: '40%'
+});
+anime ({
+    targets: '#reaction',
+    backgroundImage: 'linear-gradient(to right, hsla(0, 100%, 67%, 0.2) 80%, hsla(0, 100%, 67%, 0.1) 80%)',
+    duration: 400,
+    easing: 'linear'
+});
+anime ({
+    targets: '#memory',
+    backgroundImage: 'linear-gradient(to right, hsla(39, 100%, 56%, 0.2) 92%, hsla(39, 100%, 56%, 0.1) 92%)',
+    duration: 460,
+    easing: 'linear'
+});
+anime ({
+    targets: '#verbal',
+    backgroundImage: 'linear-gradient(to right, hsla(166, 100%, 37%, 0.2) 61%, hsla(166, 100%, 37%, 0.1) 61%)',
+    duration: 305,
+    easing: 'linear'
+});
+anime ({
+    targets: '#visual',
+    backgroundImage: 'linear-gradient(to right, hsla(234, 85%, 45%, 0.2) 72%, hsla(234, 85%, 45%, 0.1) 72%)',
+    duration: 360,
+    easing: 'linear'
+});
